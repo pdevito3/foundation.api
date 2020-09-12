bd9ae839dea85902de89099a4caedd838591eed2
Paul DeVito <pdevito3@gmail.com>
2020-09-12 00:21:53 -0400

    CurrentUserService and Interface

---
+++ templates/FoundationApi/WebApi/Services/CurrentUserService.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApi.Services
+{
+    using Application.Interfaces;
+    using Microsoft.AspNetCore.Http;
+    using System.Security.Claims;
+
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
