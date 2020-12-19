cb882aae446f325fd528c8d71583e215baf3bf1a
Paul DeVito <pdevito3@gmail.com>
2020-12-19 15:11:14 -0500

    Serilog updates for base of craftsman builds
    is not everything and will produce a broken template if used with just foundation

--- templates/FoundationApi/WebApi/Program.cs
+++ templates/FoundationApi/WebApi/Program.cs
@@ -2,7 +2,10 @@ namespace WebApi
 {
     using Autofac.Extensions.DependencyInjection;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using System;
     using System.IO;
     using System.Reflection;
 
@@ -10,11 +13,41 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var myEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var appSettings = myEnv == null ? $"appsettings.json" : $"appsettings.{myEnv}.json";
+
+            //Read Configuration from appSettings
+            var config = new ConfigurationBuilder()
+                .AddJsonFile(appSettings)
+                .Build();
+
+            //Initialize Logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting application.");
+                CreateHostBuilder(args)
+                    .Build()
+                    .Run();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "The appication failed to start correctly.");
+                throw;
+            }
+            finally
+            {
+                Log.Information("Shutting down application.");
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
@@ -23,4 +56,5 @@ namespace WebApi
                     .UseKestrel();
                 });
     }
+}
 }
